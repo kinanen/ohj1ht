bf49b0b7f4b236c38526b3eb13409afef95c62fb
Kinanen Otso Jukka Rekori <kinanojz@jyu.fi>
2022-02-02 10:56:42 +0200

    harjoitustyön projektin lataaminenb versionhallintaan

---
+++ HarjoitustyöOtsoKinanen/Tunturilasku/Tunturilasku.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Widgets;
+
+public class Tunturilasku : PhysicsGame
+{
+    public override void Begin()
+    {
+        // Kirjoita ohjelmakoodisi tähän
+
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+}
+
+
