34774476a7d450761e3a37eee38fbbd726ead31f
Kinanen Otso Jukka Rekori <kinanojz@jyu.fi>
2022-03-03 15:56:44 +0200

    olen lisännyt pelikenttän taustan ja sen liikkeen

--- HarjoitustyöOtsoKinanen/Tunturilasku/Tunturilasku.cs
+++ HarjoitustyöOtsoKinanen/Tunturilasku/Tunturilasku.cs
@@ -14,67 +14,139 @@ public class Tunturilasku : PhysicsGame
         // Kirjoita ohjelmakoodisi tähän
         Level.Size = new Vector(1600, 900);
         SetWindowSize(1600, 900);
-        GameObject tausta = new GameObject(1600, 900);
+
+
+        GameObject tausta = new GameObject(1600, 6000);
         tausta.Image = LoadImage("Tunturilasku_tausta");
-        Add(tausta);
+        tausta.Y = (-tausta.Height / 2 + Level.Height / 2);
+        Add(tausta, -3);
+
+
+        int nopeus = 1;
+
+
+        if (tausta.Bottom >= Level.Bottom)
+        {
+            tausta.Destroy();
+            GameObject uusiTausta = new GameObject(1600, 6000);
+            uusiTausta.Image = LoadImage("Tunturilasku_tausta");
+            //uusiTausta.Y = ((Screen.Height / 2) + (uusiTausta.Height / 2));
+            Add(uusiTausta, -3);
+
+        };
+
         
-        Image hahmonKuva = LoadImage("hahmo");
-        Image hahmoVasenKuva = LoadImage("hahmo_vasen");
-        Image hahmoOikeaKuva = LoadImage("hahmo_oikea");
-        Image hahmoJarruKuva = LoadImage("hahmo_jarru");
+
+        nopeus = TaustaLiike(this, tausta, nopeus);
+
 
         PhysicsObject hahmo = new PhysicsObject(100, 200, Shape.Rectangle);
-        hahmo.Image = LoadImage("hahmo");
+        hahmo.Image = LoadImage("hahmo"); ;
         Add(hahmo);
         hahmo.LinearDamping = 0.999;
 
-        Keyboard.Listen(Key.Right, ButtonState.Down, LiikutaPelaajaaO, "Pelaajaa liikutetaan oikealle", new Vector(1000, 0));
-        Mouse.Listen(MouseButton.Left, ButtonState.Down, delegate ()
-          {
-              hahmo.Push((Mouse.PositionOnWorld - hahmo.Position) * 6);
-              if (hahmo.Position.Y - Mouse.PositionOnWorld.Y < 0)
-              {
-                  hahmo.Image = LoadImage("hahmo_jarru");
-              }
-              else if(hahmo.Position.X - Mouse.PositionOnWorld.X < 0)
-              {
-                  hahmo.Image = LoadImage("hahmo_oikea");
-              }
-              else
-              {
-                  hahmo.Image = LoadImage("hahmo_vasen");
-              }
 
-          }
-           , null);
-        Mouse.Listen(MouseButton.Left, ButtonState.Released, delegate ()
-             {
-                 hahmo.Image = LoadImage("hahmo");
-    ;
-             }
-        , null);
-
-        void LiikutaPelaajaaO(Vector vector)
-        {
-            hahmo.Push(vector);
-        }
+        Ohjaus(this, hahmo);
 
 
 
-
-        /*        List<PhysicsObject> esteet = new List<PhysicsObject>();
-
-                LisaaEste();
-
-                List<PhysicsObject> viholliset = new List<PhysicsObject>();
-
-                LisaaVihollinen();
-        */
 
 
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
     }
+
+
+    /*Tämä aliohjelma sisältää pelihahmon ohjauksen hiirellä ja nuolinäppäimillä.
+    */
+    private static void Ohjaus(Game peli, PhysicsObject hahmo)
+    {
+        Image hahmonKuva = LoadImage("hahmo");
+        Image hahmoVasenKuva = LoadImage("hahmo_vasen");
+        Image hahmoOikeaKuva = LoadImage("hahmo_oikea");
+        Image hahmoJarruKuva = LoadImage("hahmo_jarru");
+
+
+        peli.Keyboard.Listen(Key.Right, ButtonState.Down, LiikutaPelaajaa, "Pelaajaa liikutetaan oikealle", new Vector(6000, 0), hahmoOikeaKuva);
+        peli.Keyboard.Listen(Key.Left, ButtonState.Down, LiikutaPelaajaa, "Pelaajaa liikutetaan oikealle", new Vector(-6000, 0), hahmoVasenKuva);
+        peli.Keyboard.Listen(Key.Up, ButtonState.Down, LiikutaPelaajaa, "Pelaajaa liikutetaan oikealle", new Vector(0, 6000), hahmoJarruKuva);
+        peli.Keyboard.Listen(Key.Down, ButtonState.Down, LiikutaPelaajaa, "Pelaajaa liikutetaan oikealle", new Vector(0, -6000), hahmonKuva);
+
+
+        void LiikutaPelaajaa(Vector vector, Image kuva)
+        {
+            hahmo.Push(vector);
+            hahmo.Image = kuva;
+        }
+
+
+        peli.Mouse.Listen(MouseButton.Left, ButtonState.Down, delegate ()
+        {
+            hahmo.Push((peli.Mouse.PositionOnWorld - hahmo.Position) * 20);
+            if (hahmo.Position.Y - peli.Mouse.PositionOnWorld.Y < 0)
+            {
+                hahmo.Image = hahmoJarruKuva;
+            }
+            else if (hahmo.Position.X - peli.Mouse.PositionOnWorld.X < 0)
+            {
+                hahmo.Image = hahmoOikeaKuva;
+            }
+            else
+            {
+                hahmo.Image = hahmoVasenKuva;
+            }
+
+        }
+           , null);
+
+        //Palauttaa pelaajan hahmon "suoraan" lasku asentoon
+        peli.Mouse.Listen(MouseButton.Left, ButtonState.Released, delegate ()
+        {
+            hahmo.Image = hahmonKuva;
+        }
+        , null);
+    }
+
+    //Aliohjelma pyörittää taustakuvaa, ja lisää taustan nopeutta, sekä palauttaa int arvona nopeuden pääohjelmaan. 
+    private static int TaustaLiike (Game peli, GameObject tausta, int nopeus)
+    {
+        double kierros = 5;
+                
+
+        Timer taustaAjastin = new Timer();
+        Timer taustanNopeus = new Timer();
+
+
+        taustaAjastin.Interval = 0.01;
+
+
+        taustanNopeus.Interval = kierros;
+        taustanNopeus.Start();
+
+
+        taustaAjastin.Start();
+        taustaAjastin.Timeout += delegate (){ tausta.Y += nopeus; };
+
+
+        taustanNopeus.Timeout += delegate () { nopeus++; };
+
+ 
+        /*if ( tausta.Bottom >= peli.Level.Bottom )
+        {
+            tausta.Destroy();
+            GameObject uusiTausta = new GameObject(1600, 6000);
+            uusiTausta.Image = LoadImage("Tunturilasku_tausta");
+            //uusiTausta.Y = ((Screen.Height / 2) + (uusiTausta.Height / 2));
+            peli.Add(uusiTausta, -3);
+            
+        };
+        */
+        return nopeus;
+    }
+
+
+
+
 
 }
 
